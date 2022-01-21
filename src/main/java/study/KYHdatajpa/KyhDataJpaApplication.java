de14cdbba6e82118f2d572bf7fd87ae62a459787
Gunwoong-Heo <gunwoong1129@gmail.com>
2022-01-21 23:44:21 +0900

    Initial commit

---
+++ src/main/java/study/KYHdatajpa/KyhDataJpaApplication.java
@@ -0,0 +1,13 @@
+package study.KYHdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyhDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyhDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/KYHdatajpa/KyhDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package study.KYHdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyhDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
