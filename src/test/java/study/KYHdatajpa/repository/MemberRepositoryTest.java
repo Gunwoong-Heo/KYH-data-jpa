3a95f1892de48427d0c99ae61e1f56de29d3bbc2
Gunwoong-Heo <gunwoong1129@gmail.com>
2022-01-23 03:02:47 +0900

    `MemberRepositoryTest` 추가

---
+++ src/test/java/study/KYHdatajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package study.KYHdatajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.KYHdatajpa.entity.Member;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+        // Optional로 반환되기 때문에 원칙적으로는 null 체크를 해야한다. 여기서는 예제라서 생략..
+        // `.get()`으로 들어가보면 null 일때 `NoSuchElementException`이 터지는 것을 확인할 수 있다.
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+
+    }
+
+}
