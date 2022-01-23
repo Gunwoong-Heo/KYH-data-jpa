8ab5be3ff090d589fd6219ccb249d38606f1b72a
Gunwoong-Heo <gunwoong1129@gmail.com>
2022-01-24 02:08:13 +0900

    `MemberDto` 추가

---
+++ src/main/java/study/KYHdatajpa/dto/MemberDto.java
@@ -0,0 +1,19 @@
+package study.KYHdatajpa.dto;
+
+import lombok.Data;
+
+// 엔티티에는 `@Data`를 웬만해서는 쓰면안된다. -> Getter,Setter 등등 다 들어가 있기 때문에
+// 단순 조회용 dto에서는 사용 가능
+@Data
+public class MemberDto {
+
+    private Long id;
+    private String username;
+    private String teamName;
+
+    public MemberDto(Long id, String username, String teamName) {
+        this.id = id;
+        this.username = username;
+        this.teamName = teamName;
+    }
+}
