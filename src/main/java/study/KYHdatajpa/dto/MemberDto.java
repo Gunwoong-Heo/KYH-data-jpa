0af4d0c6a95670e5b6523d8b678e26ad0fcc6efb
Gunwoong-Heo <gunwoong1129@gmail.com>
2022-01-25 04:55:21 +0900

    member 객체를 파라미터로 받는 생성자 추가

--- src/main/java/study/KYHdatajpa/dto/MemberDto.java
+++ src/main/java/study/KYHdatajpa/dto/MemberDto.java
@@ -1,6 +1,7 @@
 package study.KYHdatajpa.dto;
 
 import lombok.Data;
+import study.KYHdatajpa.entity.Member;
 
 // 엔티티에는 `@Data`를 웬만해서는 쓰면안된다. -> Getter,Setter 등등 다 들어가 있기 때문에
 // 단순 조회용 dto에서는 사용 가능
@@ -15,5 +16,10 @@ public class MemberDto {
         this.id = id;
         this.username = username;
         this.teamName = teamName;
+    }
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
     }
 }
