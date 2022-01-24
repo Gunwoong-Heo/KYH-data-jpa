1dd0f9bb5f0b3819e10fc30c43e095d8edf79d53
Gunwoong-Heo <gunwoong1129@gmail.com>
2022-01-24 14:12:14 +0900

    `MemberJpaRepository` 추가

--- src/main/java/study/KYHdatajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/KYHdatajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,8 @@ import study.KYHdatajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -17,9 +19,54 @@ public class MemberJpaRepository {
         return member;
     }
 
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
+
     public Member find(Long id) {
         return em.find(Member.class, id);
     }
 
+    public List<Member> findByUsernameAndAgeGreaterThan(String name, int age) {
+        return em.createQuery("select m from Member m where m.username =:name and m.age > :age")  // 실제로 호출되기 전까지는 문법오류를 알 수가 없다. -> 주의할것!  반대예시로는 `NamedQuery`가 있다.
+                .setParameter("name", name)
+                .setParameter("age", age)
+                .getResultList();
+    }
+
+    public List<Member> findByUsername(String username) {
+        return em.createNamedQuery("Member.findByUsername", Member.class)
+                .setParameter("username", username)
+                .getResultList();
+    }
+
+    public List<Member> findByPage(int age, int offset, int limit) {
+        return em.createQuery("select m from Member m where m.age = :age order by m.username desc", Member.class)
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public long totalCount(int age) {
+        return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
 
 }
